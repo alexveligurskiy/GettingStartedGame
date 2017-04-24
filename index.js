cfa179da683eb453b4689d23451f00497701aaaa
1 <a1@MacBookPro.local>
2017-04-24 14:09:05 +0300

    Demo

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 5000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/VirusGame/index.html');
+});
+
+
+/*
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -14,4 +24,14 @@ app.get('/', function(request, response) {
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+*/
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
